399d4e78efca9fd2b9b121f9fb3109cbf5b22ba9
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-03-16 22:36:04 +0400

    feature(skills): implement logic into skills component

--- src/app/features/skills/skills.ts
+++ src/app/features/skills/skills.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SKILLS_DATA } from './config/skills.config';
+import { Skill } from './models/skills.model';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Skills {}
+export class Skills {
+  public readonly skills: Skill[] = SKILLS_DATA;
+
+  public readonly duplicatedSkills: Skill[] = [...SKILLS_DATA, ...SKILLS_DATA];
+}
