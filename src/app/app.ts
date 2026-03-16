e625e25a807f5c773f3f28e2a480c15ea3724a71
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-03-16 22:02:56 +0400

    feature(structure): implement all components into main

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, signal, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './core/layout/footer/footer';
+import { Contact } from './features/contact/contact';
+import { Education } from './features/education/education';
+import { Hero } from './features/hero/hero';
+import { Projects } from './features/projects/projects';
+import { Skills } from './features/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Hero, Skills, Projects, Education, Contact, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('new-porfolio');
+  protected projectsRef = viewChild<ElementRef>('projectsRef');
+
+  public scrollToProjects(): void {
+    this.projectsRef()?.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
