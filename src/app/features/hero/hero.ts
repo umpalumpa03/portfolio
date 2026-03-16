915ae812f38bbdd67446cebf7462c9c41aef0b1c
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-03-16 23:19:48 +0400

    feature(hero-img): optimize hero img and add download cv

--- src/app/features/hero/hero.ts
+++ src/app/features/hero/hero.ts
@@ -1,14 +1,22 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, output, OutputEmitterRef } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.html',
+  imports: [NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Hero {
   public readonly viewProjects = output<void>();
 
   public downloadCV(): void {
-    console.log('Download CV clicked');
+    const link = document.createElement('a');
+    link.href = 'assets/docs/Ucha_Sephiskveradze_CV.pdf';
+    link.download = 'Ucha_Sephiskveradze_CV.pdf';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 }
